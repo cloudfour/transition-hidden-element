b0f1886d6b734f3ef157eb4545503067333da587
Paul Hebert <paul@cloudfour.com>
2019-12-24 13:55:10 -0800

    Write note about transition testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,21 @@
 const transitionHiddenElement = require('.');
 
+/**
+ * We test the methods exposted by `transitionHiddenElement`
+ *
+ * Unfortunately we can't accurately test whether transitions are running
+ * Jest relies on jsdom for dom manipulation, which does not support transitions
+ * @see https://github.com/jsdom/jsdom/issues/1781
+ */
+
 test('Hidden attribute is removed when shown', () => {
-  document.body.innerHTML = `
-    <div class="test js-test" hidden>Hidden text</div>
-  `;
+  document.body.innerHTML = `<div class="js-test" hidden>Hidden text</div>`;
 
   const testElement = document.querySelector('.js-test');
 
   const transitioner = transitionHiddenElement({
-    element: testElement
+    element: testElement,
+    visibleClass: 'is-open'
   });
 
   transitioner.show();
@@ -16,19 +23,18 @@ test('Hidden attribute is removed when shown', () => {
   expect(testElement.getAttribute('hidden')).toBe(null);
 });
 
+
 test('Hidden attribute is added when hidden', () => {
-  document.body.innerHTML = `
-    <div class="test js-test">Visible text</div>
-  `;
+  document.body.innerHTML = `<div class="js-test">Visible text</div>`;
 
   const testElement = document.querySelector('.js-test');
 
   const transitioner = transitionHiddenElement({
-    element: testElement
+    element: testElement,
+    visibleClass: 'is-open'
   });
 
   transitioner.hide();
 
   expect(testElement.getAttribute('hidden')).toBe('');
 });
-
