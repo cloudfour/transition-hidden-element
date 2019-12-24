d8ef4f1c2248fa7623e582f9a6d2590c7d70aa6e
Paul Hebert <paul@cloudfour.com>
2019-12-24 10:23:51 -0800

    Begin stubbing out `show` tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-const transitionHiddenElement = () => {
-  return 'test value';
+const transitionHiddenElement = ({element}) => {
+  return {
+    /**
+     * Show the element
+     */
+    show() {
+      element.removeAttribute('hidden');
+    }
+  };
 }
 
 module.exports = transitionHiddenElement;
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,17 @@
 const transitionHiddenElement = require('.');
 
-test('validates our testing setup', () => {
-  expect(transitionHiddenElement()).toBe('test value');
+test('Hidden attribute is removed when shown', () => {
+  document.body.innerHTML = `
+    <div class="test js-test" hidden>Hidden text</div>
+  `;
+
+  const testElement = document.querySelector('.js-test');
+
+  const transitioner = transitionHiddenElement({
+    element: testElement
+  });
+
+  transitioner.show();
+
+  expect(testElement.getAttribute('hidden')).toBe(null);
 });
