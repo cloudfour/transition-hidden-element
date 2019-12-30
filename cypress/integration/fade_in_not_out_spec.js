0154ba4b379f397b42339813e359f1586348d8fb
Paul Hebert <paul@cloudfour.com>
2019-12-30 14:23:52 -0800

    Write new tests

---
+++ cypress/integration/fade_in_not_out_spec.js
@@ -0,0 +1,90 @@
+const opacityIsTransitioning = element => {
+  const opacity = window.getComputedStyle(element).getPropertyValue('opacity');
+  return opacity > 0 && opacity < 1;
+}
+
+describe('Fade In But Not Out', function() {
+  it('Showing', function() {
+    cy.visit('/').then(function(contextWindow){
+      cy.log('Check initial state');
+      cy.get('.js-fade-in').should('have.attr', 'hidden', 'hidden');
+
+      cy.log('Trigger `show()`');
+      cy.get('.js-show-fade-in').click();
+
+      cy.log('Check that hidden has been toggled');
+      cy.get('.js-fade-in').should('not.have.attr', 'hidden');
+
+      cy.log('Wait for transition to begin');
+      cy.wait(100);
+
+      cy.log('Confirm element is transitioning');
+      cy.wrap({transitioning: opacityIsTransitioning})
+        .invoke('transitioning', contextWindow.document.querySelector('.js-fade-in'))
+        .should('be', true);
+    });
+  });
+
+  it('Hiding', function() {
+    cy.visit('/').then(function(contextWindow){
+      cy.log('Override initial state');
+      cy.get('.js-fade-in').then(fader => {
+        fader[0].removeAttribute('hidden');
+        fader[0].classList.add('is-shown');
+      });
+
+      cy.log('Confirm state override was successful');
+      cy.get('.js-fade-in').should('not.have.attr', 'hidden');
+
+      cy.log('Trigger `hide()`');
+      cy.get('.js-hide-fade-in').click();
+
+      cy.log('Confirm `hidden` is removed immediately');
+      cy.get('.js-fade-in').should('have.attr', 'hidden');
+
+      cy.log('Wait for when transition would normally be in progress');
+      cy.wait(100);
+
+      cy.log('Confirm element is not transitioning');
+      cy.wrap({transitioning: opacityIsTransitioning})
+        .invoke('transitioning', contextWindow.document.querySelector('.js-fade-in'))
+        .should('be', false);
+    });
+  });
+
+  it('Toggling', function() {
+    cy.visit('/').then(function(contextWindow){
+      cy.log('Check initial state');
+      cy.get('.js-fade-in').should('have.attr', 'hidden');
+
+      cy.log('Trigger `toggle()`');
+      cy.get('.js-toggle-fade-in').click();
+
+      cy.log('Confirm the hidden attribute has been removed');
+      cy.get('.js-fade-in').should('not.have.attr', 'hidden');
+
+      cy.log('Wait for transition to begin');
+      cy.wait(100);
+
+      cy.log('Confirm element is transitioning');
+      cy.wrap({transitioning: opacityIsTransitioning})
+        .invoke('transitioning', contextWindow.document.querySelector('.js-fade-in'))
+        .should('be', true);
+
+      cy.log('Trigger another `toggle()`');
+      cy.get('.js-toggle-fade-in').click();
+
+      cy.log('Confirm `hidden` is removed immediately');
+      cy.get('.js-fade-in').should('have.attr', 'hidden');
+
+      cy.log('Wait for when transition would normally be in progress');
+      cy.wait(100);
+
+      cy.log('Confirm element is not transitioning');
+      cy.wrap({transitioning: opacityIsTransitioning})
+        .invoke('transitioning', contextWindow.document.querySelector('.js-fade-in'))
+        .should('be', false);
+    });
+  });
+});
+
--- cypress/integration/timeout_spec.js
+++ cypress/integration/timeout_spec.js
@@ -3,24 +3,24 @@ const opacityIsTransitioning = element => {
   return opacity > 0 && opacity < 1;
 }
 
-describe('Simple Fade', function() {
+describe('Fade With Timeout HideMode', function() {
   it('Showing', function() {
     cy.visit('/').then(function(contextWindow){
       cy.log('Check initial state');
-      cy.get('.js-simple-fade').should('have.attr', 'hidden', 'hidden');
+      cy.get('.js-fade-out-timeout').should('have.attr', 'hidden', 'hidden');
 
       cy.log('Trigger `show()`');
-      cy.get('.js-show-simple-fade').click();
+      cy.get('.js-show-fade-out-timeout').click();
 
       cy.log('Check that hidden has been toggled');
-      cy.get('.js-simple-fade').should('not.have.attr', 'hidden');
+      cy.get('.js-fade-out-timeout').should('not.have.attr', 'hidden');
 
       cy.log('Wait for transition to begin');
       cy.wait(100);
 
       cy.log('Confirm element is transitioning');
       cy.wrap({transitioning: opacityIsTransitioning})
-        .invoke('transitioning', contextWindow.document.querySelector('.js-simple-fade'))
+        .invoke('transitioning', contextWindow.document.querySelector('.js-fade-out-timeout'))
         .should('be', true);
     });
   });
@@ -28,77 +28,77 @@ describe('Simple Fade', function() {
   it('Hiding', function() {
     cy.visit('/').then(function(contextWindow){
       cy.log('Override initial state');
-      cy.get('.js-simple-fade').then(fader => {
+      cy.get('.js-fade-out-timeout').then(fader => {
         fader[0].removeAttribute('hidden');
         fader[0].classList.add('is-shown');
       });
 
       cy.log('Confirm state override was successful');
-      cy.get('.js-simple-fade').should('not.have.attr', 'hidden');
+      cy.get('.js-fade-out-timeout').should('not.have.attr', 'hidden');
 
       cy.log('Trigger `hide()`');
-      cy.get('.js-hide-simple-fade').click();
+      cy.get('.js-hide-fade-out-timeout').click();
 
       cy.log('Wait for transition to begin');
       cy.wait(100);
 
       cy.log('Confirm element is transitioning');
       cy.wrap({transitioning: opacityIsTransitioning})
-        .invoke('transitioning', contextWindow.document.querySelector('.js-simple-fade'))
+        .invoke('transitioning', contextWindow.document.querySelector('.js-fade-out-timeout'))
         .should('be', true);
 
-      cy.log('Confirm `hidden` is not added during the transition');
-      cy.get('.js-simple-fade').should('not.have.attr', 'hidden');
+      cy.log('Confirm `hidden` is not added until after the timeout');
+      cy.get('.js-fade-out-timeout').should('not.have.attr', 'hidden');
 
-      cy.log('Wait for transition to end');
+      cy.log('Wait for timeout to end');
       cy.wait(300);
 
-      cy.log('Confirm `hidden` is added when the transition ends');
-      cy.get('.js-simple-fade').should('have.attr', 'hidden');
+      cy.log('Confirm `hidden` is added when the timeout ends');
+      cy.get('.js-fade-out-timeout').should('have.attr', 'hidden');
     });
   });
 
   it('Toggling', function() {
     cy.visit('/').then(function(contextWindow){
       cy.log('Check initial state');
-      cy.get('.js-simple-fade').should('have.attr', 'hidden');
+      cy.get('.js-fade-out-timeout').should('have.attr', 'hidden');
 
       cy.log('Trigger `toggle()`');
-      cy.get('.js-toggle-simple-fade').click();
+      cy.get('.js-toggle-fade-out-timeout').click();
 
       cy.log('Confirm the hidden attribute has been removed');
-      cy.get('.js-simple-fade').should('not.have.attr', 'hidden');
+      cy.get('.js-fade-out-timeout').should('not.have.attr', 'hidden');
 
       cy.log('Wait for transition to begin');
       cy.wait(100);
 
       cy.log('Confirm element is transitioning');
       cy.wrap({transitioning: opacityIsTransitioning})
-        .invoke('transitioning', contextWindow.document.querySelector('.js-simple-fade'))
+        .invoke('transitioning', contextWindow.document.querySelector('.js-fade-out-timeout'))
         .should('be', true);
 
       cy.log('Wait for transition to end');
       cy.wait(300);
 
       cy.log('Trigger another `toggle()`');
-      cy.get('.js-toggle-simple-fade').click();
+      cy.get('.js-toggle-fade-out-timeout').click();
 
       cy.log('Wait for transition to begin');
       cy.wait(100);
 
       cy.log('Confirm element is transitioning');
       cy.wrap({transitioning: opacityIsTransitioning})
-        .invoke('transitioning', contextWindow.document.querySelector('.js-simple-fade'))
+        .invoke('transitioning', contextWindow.document.querySelector('.js-fade-out-timeout'))
         .should('be', true);
 
-      cy.log('Confirm `hidden` is not added during the transition');
-      cy.get('.js-simple-fade').should('not.have.attr', 'hidden');
+      cy.log('Confirm `hidden` is not added during the timeout');
+      cy.get('.js-fade-out-timeout').should('not.have.attr', 'hidden');
 
       cy.log('Wait for transition to end');
       cy.wait(300);
 
-      cy.log('Confirm `hidden` is added when the transition ends');
-      cy.get('.js-simple-fade').should('have.attr', 'hidden');
+      cy.log('Confirm `hidden` is added when the timeout ends');
+      cy.get('.js-fade-out-timeout').should('have.attr', 'hidden');
     });
   });
 });
